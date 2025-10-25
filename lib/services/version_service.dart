d9824daf1c53c72ece8ee21362dc080e81c908a9
Mentega <mentegago@gmail.com>
2025-10-26 02:00:32 +0900

    Bump version to 22

--- lib/services/version_service.dart
+++ lib/services/version_service.dart
@@ -23,7 +23,7 @@ class VersionInfo {
 
 class VersionService {
   static const String _versionUrl = 'https://cf21-config.nnt.gg/version.json';
-  static const int _clientVersion = 21; // Current client version
+  static const int _clientVersion = 22; // Current client version
 
   static Future<VersionInfo?> fetchVersionInfo() async {
     try {
